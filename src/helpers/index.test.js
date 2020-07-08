d59e052a7f295ce5be90321290fee469f16eb6b4
Charles Kubiak <charleskubiak@Charless-MacBook-Pro.local>
2020-07-08 16:55:51 -0500

    completed letter match helper function

---
+++ src/helpers/index.test.js
@@ -0,0 +1,19 @@
+import { getLetterMatchCount } from './index'
+
+describe('getLetterMatchCount', () => {
+  const secretWord = 'party'
+  it('returns the correct count when there are no matching letters', () => {
+    const letterMatchCount = getLetterMatchCount('bones', secretWord)
+    expect(letterMatchCount).toBe(0)
+  })
+
+  it('returns the correct count when there are three matching letters', () => {
+    const letterMatchCount = getLetterMatchCount('train', secretWord)
+    expect(letterMatchCount).toBe(3)
+  })
+
+  it('returns the correct count when there are duplicate letters in the guess', () => {
+    const letterMatchCount = getLetterMatchCount('tarts', secretWord)
+    expect(letterMatchCount).toBe(3)
+  })
+})
